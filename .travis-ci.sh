06e3eb99cb3ca64bb88c81691a1974a751d1d18d
Peter Newman <peternewman@users.noreply.github.com>
2016-09-28 16:50:56 +0100

    Add some more checks to Travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -3,10 +3,14 @@ set -e # halt script on error
 
 HTMLPROOFER_OPTIONS="./_site --check-html --report-missing-names --log-level=:debug"
 
+bundle exec jekyll doctor
+bundle exec jekyll build
+
 if [[ $TASK = 'htmlproofer' ]]; then
-  bundle exec jekyll build
   bundle exec htmlproofer $HTMLPROOFER_OPTIONS --disable-external
 elif [[ $TASK = 'htmlproofer-external' ]]; then
-  bundle exec jekyll build
   bundle exec htmlproofer $HTMLPROOFER_OPTIONS
 fi
+
+# Validate GH Pages DNS setup
+bundle exec github-pages health-check
